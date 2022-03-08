977fa81fdd2ba068241aac3985f224bc526ecbad
Vamsi-Teeparthi <vamsi5256@gmail.com>
2022-03-08 13:15:42 +0100

    Unit Test case for islower string added

--- Unit_Test_String_Methods_Application.py
+++ Unit_Test_String_Methods_Application.py
@@ -7,5 +7,8 @@ class TestStringMethods(unittest.TestCase):
     def test_capitalize_string(self):
         output = string_methods.capitalize_string("software testing")
         self.assertEqual(output, "Software testing")
+    def test_islower_string(self):
+        output = string_methods.islower_string("software testing")
+        self.assertTrue(output, "software testing")
 if __name__ == '__main__':
     unittest.main()
