6db985b2143354be4041f8403112a560b7da29ae
sadasiva111 <sadasiva6699@gmail.com>
2022-03-08 12:04:30 +0100

    init

---
+++ String_Methods.py
@@ -0,0 +1,23 @@
+#string methods in python
+class string_methods_application:
+
+    def capitalize_string(my_string):
+        return my_string.capitalize()
+
+    def islower_string(my_string):
+        return my_string.islower()
+
+    def replace_string(my_string):
+        return my_string.replace("software", "unit")
+
+    def count_string(my_string):
+        return my_string.count("software")
+
+    def casefold_string(my_string):
+        return my_string.casefold()
+
+    def endswith_string(my_string):
+        my_string = "Software Testing."
+        return my_string.endswith(".")
+    
+
