f5b402052d47ae64e9c2e9e06fa0607cff7733cf
SreeChandan13 <chandu7264530046@gmail.com>
2022-03-08 13:02:24 +0000

    Added Test Case for Count String

--- Unit_Test_String_Methods_Application.py
+++ Unit_Test_String_Methods_Application.py
@@ -11,10 +11,14 @@ class TestStringMethods(unittest.TestCase):
     def test_islower_string(self):
         output = string_methods.islower_string("software testing")
         self.assertTrue(output, "software testing")
-        
+
     def test_replace_string(self):
         output = string_methods.replace_string("software testing")
         self.assertEqual(output, "unit testing")
+
+    def test_count_string(self):
+        output = string_methods.count_string("software testing")
+        self.assertEqual(output, 1)
     
 if __name__ == '__main__':
     unittest.main()
